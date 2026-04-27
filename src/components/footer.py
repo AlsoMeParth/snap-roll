c29ef3b34bd78b63411df3b195e52a2ade09c922
AlsoMeParth <opxplays@gmail.com>
2026-04-27 12:36:19 +0530

    streamlit-setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown(f"""
+            <div style = "margin-top:2rem; display:flex; gap:6px; justify-content:center; item-align:center">
+                <p style = "font-weight:bold; color:white">🔥 AI POWERED SMART ATTENDANCE SYSTEM</p>
+            </div>
+
+
+                """, unsafe_allow_html=True)
